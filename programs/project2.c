5e4dba521a022e5f4a37d0dd1661269859520515
XarthDaichi <diegoqahh@gmail.com>
2023-06-11 01:34:40 -0600

    finished implementing tree

--- programs/project2.c
+++ programs/project2.c
@@ -9,20 +9,26 @@
 #include <pthread.h>
 #include <time.h>
 #include <errno.h>
+#include <string.h>
 
+// mutex for each element in the solution_array for entering
 static pthread_mutex_t solution_mutex[256];
 
+// solution and aux for array sorting to maintain right byte
 static unsigned long solution_array[256] = {0};
 static int solution_aux[256];
 
+// amount of readers that are going to be used
 static int readers_num;
 
+// original file info
 static char *filepath;
 static long filelen;
 
-
+// new files info
 static char *filename;
 
+// methods for sorting array (radixsort)
 unsigned long get_max() {
     unsigned long max = solution_array[0];
     for (int i = 1; i < 256; i++) {
@@ -59,6 +65,8 @@ void radix_sort() {
     for (int pos_dig = 1; max / pos_dig; pos_dig *= 10) count_sort(pos_dig);
 }
 
+
+// method for reading file
 void *reading_file(void* input_num) {
     int reader_num = *((int*) input_num);
     long start= (filelen / readers_num) * reader_num;
@@ -79,6 +87,127 @@ void *reading_file(void* input_num) {
     }
     fclose(fileptr);
     pthread_exit((void*)0);
+}
+
+
+// things for tree creation
+struct Node {
+    int byte;
+    unsigned long amount_of_byte;
+    int path;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* create_node(int inserting_byte, unsigned long amount) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->byte = inserting_byte;
+    new_node->amount_of_byte = amount;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+struct Node* create_node_not_byte(int amount) {
+    return create_node(-1, amount);
+}
+
+int amount_of_zeros() {
+    int counter = -1;
+    for (int i = 0; i < 256; i++) {
+        if (solution_array[i] != 0) break;
+        counter++;
+    }
+    return counter;
+}
+
+struct Node* create_tree() {
+    int amount_of_bytes_zero = amount_of_zeros();
+
+    struct Node* non_byte_nodes[255] = { NULL };
+    int nb_total = 0, last_nb_add = 0;
+
+    non_byte_nodes[nb_total] = create_node_not_byte(0);
+
+    non_byte_nodes[nb_total]->left = create_node(solution_aux[amount_of_bytes_zero + 1], solution_array[amount_of_bytes_zero + 1]);
+    non_byte_nodes[nb_total]->right = create_node(solution_aux[amount_of_bytes_zero + 2], solution_array[amount_of_bytes_zero + 2]);
+    non_byte_nodes[nb_total]->amount_of_byte = solution_array[amount_of_bytes_zero + 1] + solution_array[amount_of_bytes_zero + 2];
+
+    int all_loaded = 0;
+    int i = amount_of_bytes_zero + 3;
+
+    while (i < 256 || !all_loaded) {
+        if (i < 255) {
+            if (non_byte_nodes[last_nb_add]->amount_of_byte < solution_array[i]) {
+                if (last_nb_add > 0 && non_byte_nodes[last_nb_add + 1]->amount_of_byte < solution_array[i]) {
+                    non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add + 1]->amount_of_byte + non_byte_nodes[last_nb_add]->amount_of_byte);
+                    non_byte_nodes[nb_total]->left = non_byte_nodes[last_nb_add];
+                    non_byte_nodes[nb_total]->right = non_byte_nodes[last_nb_add + 1];
+                    last_nb_add += 2;
+                } else {
+                    non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add]->amount_of_byte + solution_array[i]);
+                    non_byte_nodes[nb_total]->left = non_byte_nodes[last_nb_add];
+                    non_byte_nodes[nb_total]->right = create_node(solution_aux[i], solution_array[i]);
+                    last_nb_add++;
+                    i++;
+                }
+            } else {
+                if (non_byte_nodes[last_nb_add]->amount_of_byte < solution_array[i + 1]) {
+                    non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add]->amount_of_byte + solution_array[i]);
+                    non_byte_nodes[nb_total]->left = create_node(solution_aux[i], solution_array[i]);
+                    non_byte_nodes[nb_total]->right = non_byte_nodes[last_nb_add];
+                    last_nb_add++;
+                    i++;
+                } else {
+                    non_byte_nodes[++nb_total] = create_node_not_byte(solution_array[i] + solution_array[i + 1]);
+                    non_byte_nodes[nb_total]->left = create_node(solution_aux[i], solution_array[i]);
+                    non_byte_nodes[nb_total]->right = create_node(solution_aux[i + 1], solution_array[i + 1]);
+                    i += 2;
+                }
+            }
+        }
+        else if (i == 255) {
+            if (non_byte_nodes[last_nb_add]->amount_of_byte < solution_array[i]) {
+                non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add]->amount_of_byte + solution_array[i]);
+                non_byte_nodes[nb_total]->left = non_byte_nodes[last_nb_add];
+                non_byte_nodes[nb_total]->right = create_node(solution_aux[i], solution_array[i]);
+                last_nb_add++;
+                i++;
+            } else {
+                non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add]->amount_of_byte + solution_array[i]);
+                non_byte_nodes[nb_total]->left = create_node(solution_aux[i], solution_array[i]);
+                non_byte_nodes[nb_total]->right = non_byte_nodes[last_nb_add];
+                last_nb_add++;
+                i++;
+            }
+        }
+        else {
+            non_byte_nodes[++nb_total] = create_node_not_byte(non_byte_nodes[last_nb_add + 1]->amount_of_byte + non_byte_nodes[last_nb_add]->amount_of_byte);
+            non_byte_nodes[nb_total]->left = non_byte_nodes[last_nb_add];
+            non_byte_nodes[nb_total]->right = non_byte_nodes[last_nb_add + 1];
+            last_nb_add += 2;
+        }
+        if (last_nb_add == nb_total) {
+            break;
+        }
+    }
+
+    return non_byte_nodes[nb_total];
+}
+
+void print_tree(struct Node* root, char* tab) {
+    if (root->byte != -1) {
+        printf("%s|_>%c:%lu\n",tab, root->byte, root->amount_of_byte);
+    } else {
+        char* new_tab = malloc(strlen(tab)+1+1);
+        strcpy(new_tab, tab);
+        strcat(new_tab, "\t");
+        if (tab != "") printf("%s|_>%lu\n",tab, root->amount_of_byte);
+        else printf("%s\t%lu\n",tab, root->amount_of_byte);
+        print_tree(root->left, new_tab);
+        print_tree(root->right, new_tab);
+        free(new_tab);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -135,17 +264,25 @@ int main(int argc, char *argv[]) {
     }
 
     radix_sort();
-    // printf("La cantidad de caracteres es: %ld\n", filelen);
+    printf("La cantidad de caracteres es: %lu\n", filelen);
     long total = 0;
     for (int i = 0; i < 256; i++) {
         if (solution_array[i] != 0) {
-            printf("%d aparece %d veces\n", solution_aux[i], solution_array[i]);
+            printf("%c aparece %d veces\n", solution_aux[i], solution_array[i]);
             total += solution_array[i];
         }
     }
-    // printf("La cantidad que leyo es: %ld\n", total);
+    printf("La cantidad que leyo es: %lu\n", total);
     for (int i = 0; i < 256; i++) {
         pthread_mutex_destroy(&solution_mutex[i]);
     }
+
+    if (filelen != 0) {
+        struct Node* tree_root = create_tree();
+        print_tree(tree_root, "");
+    }
+
+
+
     return 0;
 }
